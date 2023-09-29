b661354faa880481383e0084454d717c8f47fb22
pmikola <pio.mikolajczyk@gmail.com>
2023-09-29 22:17:10 +0200

    MAE,MPE,MPEd,MRE,MREh,regression

--- MMI_test.py
+++ MMI_test.py
@@ -4,14 +4,31 @@ from scipy import stats
 from scipy.stats import norm
 
 # FIMMWAVE
-dataset1 = [0.439, 0.454, 0.483, 0.469, 0.44, 0.45, 0.48, 0.472, 0.458, 0.454, 0.46, 0.456, 0.456, 0.453, 0.454, 0.455]
+datasetfi1_2 = [0.458, 0.454, 0.46, 0.456, 0.456, 0.453, 0.454, 0.455]
+datasetfi2_2 = [0.439, 0.454, 0.483, 0.469, 0.44, 0.45, 0.48, 0.472]
+
+dataset1 =  datasetfi1_2 + datasetfi2_2
 # FDTD 2d
-dataset2 = [0.448, 0.497, 0.453, 0.48, 0.45, 0.485, 0.425, 0.476, 0.497, 0.48, 0.44, 0.495, 0.495, 0.481, 0.44, 0.494]
+datasetfd1_2 = [ 0.497, 0.48, 0.44, 0.495, 0.495, 0.481, 0.44, 0.494]
+datasetfd2_2 = [0.448, 0.497, 0.453, 0.48, 0.45, 0.485, 0.425, 0.476]
+dataset2 = datasetfd1_2 + datasetfd2_2
 
 # Obliczenie procentowej różnicy między datasetami
 mean1 = np.mean(dataset1)
 mean2 = np.mean(dataset2)
 percentage_difference = abs(mean1 - mean2) / ((mean1 + mean2) / 2) * 100
+
+# Obliczenie procentowego odchylenia maksymalnego
+std_dev_max1 = (max(dataset1) - mean1) / mean1 * 100
+std_dev_max2 = (max(dataset2) - mean2) / mean2 * 100
+
+# Obliczenie MAE (Mean Absolute Error)
+mae = np.mean(np.abs(np.array(dataset1) - np.array(dataset2)))
+
+# Obliczenie MRE (Maximum Relative Error)
+max_relative_error = np.max(np.abs(np.array(dataset1) - np.array(dataset2)) / np.array(dataset1)) * 100
+# Obliczenie MPE (Mean Percentage Error)
+mpe = np.mean(((np.array(dataset1) - np.array(dataset2)) / np.array(dataset1)) * 100)
 
 distance = np.linalg.norm(np.array(dataset1) - np.array(dataset2))
 # Test t-Studenta dla dwóch niezależnych próbek
@@ -40,13 +57,42 @@ plt.ylim(0, max_y)
 plt.xlim(min_x, max_x)
 plt.show()
 
+# Analiza regresji
+slope, intercept, r_value, p_value, std_err = stats.linregress(dataset1, dataset2)
+plt.scatter(generated_samples1, generated_samples2, label='Pfwd', color='blue')
+# Linia regresji
+x = np.array(generated_samples1)
+y = slope * x + intercept
+plt.plot(x, y, color='red', label='reg line')
+
+plt.xlabel('Pfwd FIMMWAVE ')
+plt.ylabel('Pfwd FDTD ')
+plt.legend()
+plt.grid()
+
+# Wyświetlenie wyników
+
 print(f"Wartość statystyki t: {t_stat}")
 print(f"Wartość p: {p_value}")
 
-alpha = 0.05  # Poziom istotności
+alpha = 0.05  # Poziom istotności w procentach %
 if p_value < alpha and ks_p_value < alpha:
     print("Odrzucamy hipotezę zerową - Istnieje różnica między rozkładami.")
 else:
     print("Nie ma podstaw do odrzucenia hipotezy zerowej - Brak różnicy między rozkładami.")
 
-print(f"Procentowa różnica między datasetami: {percentage_difference:.2f}%")
+print(f"Współczynnik kierunkowy (slope): {slope:.4f}")
+print(f'Wyraz wolny (intercept): {intercept:.4f}')
+print(f'Współczynnik korelacji (R-squared): {r_value**2:.4f}')
+print(f'Wartość p (test istotności współczynnika kierunkowego): {p_value:.4f}')
+print(f'Błąd standardowy (std_err): {std_err:.4f}')
+print(f"Procentowa różnica między rozkładami: {percentage_difference:.2f}%")
+print(f"Procentowe odchylenie maksymalne (FIMMWAVE): {std_dev_max1:.2f}%")
+print(f"Procentowe odchylenie maksymalne (FDTD): {std_dev_max2:.2f}%")
+print(f'MAE: {mae:.4f}')
+print(f'MPE: {abs(mpe):.4f}%')
+print(f'MPE_double: {abs(mpe)*2:.4f}%')
+print(f'MRE: {max_relative_error:.4f}%')
+print(f'MRE_half: {max_relative_error/2:.4f}%')
+
+plt.show()
