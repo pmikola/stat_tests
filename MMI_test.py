9ee3186cf380c0a99ad036b617b98c663a5251a0
pmikola <pio.mikolajczyk@gmail.com>
2023-09-29 20:15:11 +0200

    % added

--- MMI_test.py
+++ MMI_test.py
@@ -7,6 +7,12 @@ from scipy.stats import norm
 dataset1 = [0.439, 0.454, 0.483, 0.469, 0.44, 0.45, 0.48, 0.472, 0.458, 0.454, 0.46, 0.456, 0.456, 0.453, 0.454, 0.455]
 # FDTD 2d
 dataset2 = [0.448, 0.497, 0.453, 0.48, 0.45, 0.485, 0.425, 0.476, 0.497, 0.48, 0.44, 0.495, 0.495, 0.481, 0.44, 0.494]
+
+# Obliczenie procentowej różnicy między datasetami
+mean1 = np.mean(dataset1)
+mean2 = np.mean(dataset2)
+percentage_difference = abs(mean1 - mean2) / ((mean1 + mean2) / 2) * 100
+
 distance = np.linalg.norm(np.array(dataset1) - np.array(dataset2))
 # Test t-Studenta dla dwóch niezależnych próbek
 t_stat, p_value = stats.ttest_ind(dataset1, dataset2)
@@ -16,7 +22,7 @@ n_samples = 1000
 mean1, std_dev1 = norm.fit(dataset1)
 mean2, std_dev2 = norm.fit(dataset2)
 generated_samples1 = np.random.normal(mean1, std_dev1, n_samples)
-generated_samples2 = np.random.norOdchylmal(mean2, std_dev2, n_samples)
+generated_samples2 = np.random.normal(mean2, std_dev2, n_samples)
 hist1, bin_edges1, _ = plt.hist(generated_samples1, bins=50, alpha=0.5, label='FIMMWAVE', color='blue')
 hist2, bin_edges2, _ = plt.hist(generated_samples2, bins=50, alpha=0.5, label='FDTD', color='orange')
 max_y = max(max(hist1), max(hist2))
@@ -42,3 +48,5 @@ if p_value < alpha and ks_p_value < alpha:
     print("Odrzucamy hipotezę zerową - Istnieje różnica między rozkładami.")
 else:
     print("Nie ma podstaw do odrzucenia hipotezy zerowej - Brak różnicy między rozkładami.")
+
+print(f"Procentowa różnica między datasetami: {percentage_difference:.2f}%")
